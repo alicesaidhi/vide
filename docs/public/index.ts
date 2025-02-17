14c36d131fce641742759ece8d76ce1bcc201d1c
Alice <166900055+alicesaidhi@users.noreply.github.com>
2025-02-17 19:23:26 +0100

    update links

--- docs/public/index.ts
+++ docs/public/index.ts
@@ -2,24 +2,21 @@ import { Cart, CartOptions, Memory, stdIo } from 'cart-luau';
 import file from "!raw-loader!/app.luau"
 import type { Inode } from "@bjorn3/browser_wasi_shim";
 import { File } from "@bjorn3/browser_wasi_shim";
-import bundle from "!raw-loader!/bundle.luau"
-// import wasm from "!raw-loader!/cart.wasm"
 
 const shared_mem = new Memory();
 let cart: Cart;
 
-// function str2ab(str) {
-//     var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-//     var bufView = new Uint16Array(buf);
-//     for (var i=0, strLen=str.length; i<strLen; i++) {
-//         bufView[i] = str.charCodeAt(i);
-//     }
-//     return buf;
-// }
-
 async function run() {
+    const frame = await fetch("https://alicesaidhi.github.io/vide/frame.luau");
+    const datatypes = await fetch("https://alicesaidhi.github.io/vide/datatypes.luau");
+    const instance = await fetch("https://alicesaidhi.github.io/vide/instance.luau");
+    const vide = await fetch("https://alicesaidhi.github.io/vide/vide.luau");
+    
+
     const fs = new Map<string, Inode>();
-    const text =  (bundle + file)
+    const text = (await instance.text()) + "\n" + (await datatypes.text()) + "\n" + (await frame.text()) + "\n" + (await vide.text()) + "\n"
+
+    console.log("added libraries")
 
     cart = new Cart(
         new CartOptions({
@@ -28,10 +25,7 @@ async function run() {
         })
     )
 
-    // console.log(result);
-    // console.log(await (await fetch("https://alicesaidhi.github.io/vide/cart.wasm")).text())
     await cart.load("https://alicesaidhi.github.io/vide/cart.wasm");
-
     const thread = cart.loadThreadFromString("meow", text + file);
 
     if (!thread.valid) {
